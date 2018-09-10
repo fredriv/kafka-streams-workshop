823f9d78ab26eb9b0d188c3230f2a62881683369
Fredrik Vraalsen <fredrik@vraalsen.no>
2018-09-10 23:04:52 +0200

    Add Scala version of exercise 3 Joins

--- src/main/scala/kafkastreams/scalaexercises/Exercise_3_Joins.scala
+++ src/main/scala/kafkastreams/scalaexercises/Exercise_3_Joins.scala
@@ -1,8 +1,16 @@
 package kafkastreams.scalaexercises
 
-import org.apache.kafka.streams.scala.StreamsBuilder
+import com.fasterxml.jackson.databind.node.{ArrayNode, ObjectNode}
+import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import kafkastreams.serdes.JsonNodeSerde
+import org.apache.kafka.streams.kstream._
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala.Serdes.String
+import org.apache.kafka.streams.scala.{StreamsBuilder, kstream}
 
 class Exercise_3_Joins {
+
+  private implicit val json = new JsonNodeSerde
 
   /**
     * Join user events with account state based on user id, producing values of the form
@@ -11,24 +19,45 @@ class Exercise_3_Joins {
     * <p>
     * For input and output topic names, see Exercise_3_JoinsTest
     */
-  def accountStateJoin(builder: StreamsBuilder): Unit = ???
+  def accountStateJoin(builder: StreamsBuilder): Unit = {
+
+  }
 
   /**
     * Join user events with account state, outputing the last visited page
     * when an account is cancelled/closed.
     */
-  def accountCancellationLastVisitedPage(builder: StreamsBuilder): Unit = ???
+  def accountCancellationLastVisitedPage(builder: StreamsBuilder): Unit = {
+
+  }
+
+  def createPageViewRelatedToAccountChangeJoiner(mapper: ObjectMapper) =
+    (accountState: JsonNode, pageView: JsonNode) => getPageViewRelatedToAccountChange(mapper, accountState, pageView)
+
+  def getPageViewRelatedToAccountChange(mapper: ObjectMapper, accountState: JsonNode, pageView: JsonNode): JsonNode = {
+    val node = mapper.createObjectNode
+
+    // TODO populate the JSON object with the expected fields. See Exercise_3_JoinsTest for details.
+
+    node
+  }
 
   /**
     * Emit an event stream of account state changes paired with
     * each pageview happening within 1000 ms of the state change.
+    *
+    * You can fill in and use the helper methods above.
     */
-  def pageViewsWithAccountStateChange(builder: StreamsBuilder): Unit = ???
+  def pageViewsWithAccountStateChange(builder: StreamsBuilder): Unit = {
+
+  }
 
   /**
     * Emit an event stream of account state changes paired with
     * a *list of* pageviews happening within 1000 ms of the state change.
     */
-  def pageViewsWithListOfAccountStateChange(builder: StreamsBuilder): Unit = ???
+  def pageViewsWithListOfAccountStateChange(builder: StreamsBuilder): Unit = {
+
+  }
 
 }
