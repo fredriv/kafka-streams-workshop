c635fba2537ce9d2efbc99c68942e02ff0dbe8a6
Fredrik Vraalsen <fredrik@vraalsen.no>
2018-09-10 23:05:46 +0200

    Add Scala solution for Exercise 3 Joins

--- src/main/scala/kafkastreams/scalaexercises/Exercise_3_Joins.scala
+++ src/main/scala/kafkastreams/scalaexercises/Exercise_3_Joins.scala
@@ -20,7 +20,12 @@ class Exercise_3_Joins {
     * For input and output topic names, see Exercise_3_JoinsTest
     */
   def accountStateJoin(builder: StreamsBuilder): Unit = {
+    val accountStateKTable = builder.table[String, String]("account-state-changelog")
+    val userEventStream = builder.stream[String, String]("user-events")
 
+    userEventStream
+      .join(accountStateKTable)((value1, value2) => value1 + "-" + value2)
+      .to("user-events-with-accountstate")
   }
 
   /**
@@ -28,7 +33,13 @@ class Exercise_3_Joins {
     * when an account is cancelled/closed.
     */
   def accountCancellationLastVisitedPage(builder: StreamsBuilder): Unit = {
+    val accountState = builder.stream[String, String]("account-state-changelog")
+    val userEventStream = builder.table[String, String]("user-events")
 
+    accountState
+      .filter((key, value) => value == "closed")
+      .join(userEventStream)((value1, value2) => value2)
+      .to("account-cancellation-last-visited-page")
   }
 
   def createPageViewRelatedToAccountChangeJoiner(mapper: ObjectMapper) =
@@ -37,7 +48,10 @@ class Exercise_3_Joins {
   def getPageViewRelatedToAccountChange(mapper: ObjectMapper, accountState: JsonNode, pageView: JsonNode): JsonNode = {
     val node = mapper.createObjectNode
 
-    // TODO populate the JSON object with the expected fields. See Exercise_3_JoinsTest for details.
+    node.set("pageTimestamp", pageView.path("timestamp"))
+    node.set("stateTimestamp", accountState.path("timestamp"))
+    node.set("page", pageView.path("page"))
+    node.set("state", accountState.path("state"))
 
     node
   }
@@ -49,7 +63,17 @@ class Exercise_3_Joins {
     * You can fill in and use the helper methods above.
     */
   def pageViewsWithAccountStateChange(builder: StreamsBuilder): Unit = {
+    val accountStateStream = builder.stream[String, JsonNode]("account-state-changelog")
+    val userEventStream = builder.stream[String, JsonNode]("user-events")
 
+    val mapper = new ObjectMapper
+
+    accountStateStream
+      .join(userEventStream)(
+        createPageViewRelatedToAccountChangeJoiner(mapper),
+        JoinWindows.of(1000)
+      )
+      .to("account-state-coinciding-pageview")
   }
 
   /**
@@ -57,7 +81,35 @@ class Exercise_3_Joins {
     * a *list of* pageviews happening within 1000 ms of the state change.
     */
   def pageViewsWithListOfAccountStateChange(builder: StreamsBuilder): Unit = {
+    val accountStateStream = builder.stream[String, JsonNode]("account-state-changelog")
+    val userEventStream = builder.stream[String, JsonNode]("user-events")
 
+    val mapper = new ObjectMapper
+
+    val aggregator = (key: String, value: JsonNode, aggregate: JsonNode) => {
+      val page = mapper.createObjectNode
+      page.set("id", value.path("page"))
+      page.set("timestamp", value.path("pageTimestamp"))
+      aggregate.path("pages").asInstanceOf[ArrayNode].add(page)
+      aggregate.asInstanceOf[ObjectNode].set("state", value.path("state"))
+      aggregate.asInstanceOf[ObjectNode].set("stateTimestamp", value.path("stateTimestamp"))
+      aggregate
+    }
+
+    def initializer: JsonNode = {
+      val result = mapper.createObjectNode
+      result.set("pages", mapper.createArrayNode)
+    }
+
+    accountStateStream
+      .join(userEventStream)(
+        createPageViewRelatedToAccountChangeJoiner(mapper),
+        JoinWindows.of(1000)
+      )
+      .groupBy((key, value) => key + "-" + value.path("state").asText + "-" + value.path("stateTimestamp").toString) // Gotcha - what happens if you use .textValue here?
+      .aggregate(initializer)(aggregator)
+      .toStream
+      .to("account-state-coinciding-pageslist")
   }
 
 }
